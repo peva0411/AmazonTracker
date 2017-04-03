3ce59b1de8ad95bb0f23b0efda59cba2748dddc9
Phil <peva0411@gmail.com>
2017-04-02 21:16:58 -0400

    Adding testing script file

---
+++ AmazonTracker.Console/Script1.fsx
@@ -0,0 +1,13 @@
+﻿
+#r@"C:\projects\AmazonTracker\packages\FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type AmazonProductPage = HtmlProvider<"https://www.amazon.com/exec/obidos/asin/B000ILIHA6">
+
+let blueDog = AmazonProductPage.Load("https://www.amazon.com/exec/obidos/asin/B000ILIHA6")
+
+blueDog.Lists.``Product details``
+
+let prodDetails = blueDog.Lists.``Product details``
+let list1 = blueDog.Lists.``List2``
